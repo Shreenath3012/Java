3ab14e446df565fc65ed0fd1d44911885fe89eb4
Shreenath Shukla <shreenathshukla30@gmail.com>
2019-10-30 13:23:26 +0530

    Adding a Simple Java Program

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		for(int i=10; i>=0; i--)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Welcome to Jenkins-Git Integration.");
+	}
+}
